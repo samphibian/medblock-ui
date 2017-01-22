a449b5789aafa5849e28e19950a5ba953c206027
Jeremy Lautman <jeremy.lautman@gmail.com>
2017-01-21 19:16:36 -0500

    Commit remaining generated files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medblockApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the medblockApp
+ */
+angular.module('medblockApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medblockApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the medblockApp
+ */
+angular.module('medblockApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
