bd5120856718aff1ae571d06a2554723ac91975e
YumAntimatter <mihzelenin@gmail.com>
2017-01-21 23:43:53 -0500

    Added login page

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medblockApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the medblockApp
+ */
+angular.module('medblockApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'Kittens'
+    ];
+  });
